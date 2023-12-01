c80d999b3d9f626a590ebff346e12a6b56852ac9
AppSeed Generated Projects <133564247+appseed-projects5@users.noreply.github.com>
2023-12-01 14:03:06 +0200

    initial commit

---
+++ home/models.py
@@ -0,0 +1,57 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
+
+# Create your models here.
+
+class UserProfile(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    #__PROFILE_FIELDS__
+
+    #__PROFILE_FIELDS__END
+
+    def __str__(self):
+        return self.user.username
+    
+    class Meta:
+        verbose_name        = _("UserProfile")
+        verbose_name_plural = _("UserProfile")
+
+#__MODELS__
+class Vendor(models.Model):
+
+    #__Vendor_FIELDS__
+    id = models.CharField(max_length=255, null=True, blank=True)
+    phone = models.CharField(max_length=255, null=True, blank=True)
+    email = models.CharField(max_length=255, null=True, blank=True)
+
+    #__Vendor_FIELDS__END
+
+    class Meta:
+        verbose_name        = _("Vendor")
+        verbose_name_plural = _("Vendor")
+
+
+class Customer(models.Model):
+
+    #__Customer_FIELDS__
+    name = models.CharField(max_length=255, null=True, blank=True)
+    phone = models.CharField(max_length=255, null=True, blank=True)
+    bio = models.CharField(max_length=255, null=True, blank=True)
+
+    #__Customer_FIELDS__END
+
+    class Meta:
+        verbose_name        = _("Customer")
+        verbose_name_plural = _("Customer")
+
+
+
+#__MODELS__END
